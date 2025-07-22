a2ecb2ae04c720c4f8903370d9f5f1d92677df73
monish <monishmass29@gmail.com>
2025-07-22 23:18:56 +0530

    fix changes

--- resources/views/backend/product/product_all.blade.php
+++ resources/views/backend/product/product_all.blade.php
@@ -57,7 +57,7 @@
                                 @else
                                 @php
                                 $amount = $item->selling_price - $item->discount_price;
-                                $discount = $item->selling_price?($amount/$item->selling_price)*100:0;
+                                $discount = $item->selling_price&&$amount?($amount/$item->selling_price)*100:0;
                                 @endphp
                                 <span class="badge rounded-pill bg-danger">{{round($discount)}}%</span>
                                 @endif
